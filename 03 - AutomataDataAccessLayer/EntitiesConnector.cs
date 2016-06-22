a1f0066ea5d3572969c32abafee260285681522b
fabriciopapa <fabriciopapa90@gmail.com>
2016-06-22 20:24:03 -0300

    Primer seteo de ambiente.

---
+++ 03 - AutomataDataAccessLayer/EntitiesConnector.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomataEntitiesLayer;
+using System.Data.Entity;
+
+namespace AutomataDataAccessLayer
+{
+    public class EntitiesConnector
+    {
+        private Exception exception;
+
+        #region SELECT
+        public AutomataEntitiesLayer.User SelectUser(string userName, string password)
+        {
+            AutomataEntitiesLayer.User user = null;
+            
+            try
+            {
+                using (AutomataEntities context = new AutomataEntities())
+                {
+                    user = context.Users.Where(u => u.UserName == userName && u.Password == password).Select(u => u).FirstOrDefault();
+                    if (user == null)
+                    {
+                        exception = new Exception("Ingrese correctamente los datos del usuario.");
+                    }
+                }
+            }
+            catch
+            {
+                exception = new Exception("Ocurrió un error al realizar login.");
+            }
+
+            if (exception != null)
+                throw exception;
+
+            return user;
+        }
+        #endregion
+
+        #region INSERT
+        public void InsertUser(AutomataEntitiesLayer.User user)
+        {
+            try
+            {
+                using (AutomataEntities context = new AutomataEntities())
+                {
+                    User dbUser = context.Users.Where(u => u.UserName == user.UserName).Select(u => u).FirstOrDefault();
+                    if (dbUser == null)
+                    {
+                        context.Users.Add(user);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        exception = new Exception("Ya existe un usuario con los datos provistos.");
+                    }
+                }
+            }
+            catch
+            {
+                exception = new Exception("Ocurrió un error al dar de alta el usuario.");
+            }
+
+            if (exception != null)
+                throw exception;
+        }
+        public void InsertProject(AutomataEntitiesLayer.User user, AutomataEntitiesLayer.Project project)
+        {
+            try
+            {
+                
+                using (AutomataEntities context = new AutomataEntities())
+                {                   
+                    if (user.Projects == null)
+                        user.Projects = new List<Project>();
+                    
+                    user.Projects.Add(project);
+
+                    context.Projects.Add(project);
+                    context.SaveChanges();
+                }
+            }
+            catch
+            {
+                exception = new Exception("Ocurrió un error al dar de alta el proyecto.");
+            }
+
+            if (exception != null)
+                throw exception;
+        }
+        #endregion
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutomataDataAccessLayer;
+using AutomataEntitiesLayer;
+
+namespace Tester
+{
+    class Program
+    {
+        static string info = "Ejecutando region: {0}";
+
+        static void Main(string[] args)
+        {
+            //InsertUser();
+            InsertProject();
+            Console.ReadLine();
+        }
+
+        static void InsertUser()
+        {
+            AutomataEntitiesLayer.User user = new AutomataEntitiesLayer.User() { UserName = "fabriciop", Password = "Password01", Name = "Fabricio", LastName = "Papa", SecretAnswer = "respuesta", Projects = null, Active = true };
+            try
+            {
+                new EntitiesConnector().InsertUser(user);
+                Console.WriteLine("Success!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void InsertProject()
+        {
+            AutomataEntitiesLayer.User user = new EntitiesConnector().SelectUser("fabriciop", "Password01");
+            AutomataEntitiesLayer.Project project = new AutomataEntitiesLayer.Project() { Name = "Proyecto2", Url = "http://www.prueba.com", Tests = null, Active = true };
+            try
+            {
+                new EntitiesConnector().InsertProject(user, project);
+                Console.WriteLine("Éxito!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
